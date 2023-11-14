80cd37c2ea624fbf007a01188c3f94038949b7f1
sangchu <didi1484@gmail.com>
2023-11-14 17:49:13 +0900

    feat: printTurn 메서드 추가
    현재 턴을 출력함

--- src/main/controller/Controller.java
+++ src/main/controller/Controller.java
@@ -1,7 +1,12 @@
 package main.controller;
 
+import main.domain.Turn;
+import main.view.OutputView;
+
 public class Controller {
     public void start(){
-
+        OutputView.printStartMessage();
+        Turn turn = new Turn();
+        OutputView.printTurn(turn);
     }
 }
