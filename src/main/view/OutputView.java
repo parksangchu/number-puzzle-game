fad8b8fa1861fe1cf9c44ba1e02830dc739d787c
sangchu <didi1484@gmail.com>
2023-11-14 21:07:37 +0900

    feat: printNumbers 메서드 추가
    숫자 리스트 출력

--- src/main/view/OutputView.java
+++ src/main/view/OutputView.java
@@ -1,14 +1,21 @@
 package main.view;
 
+import main.domain.Numbers;
 import main.domain.Turn;
 
 public class OutputView {
     private static final String START_MESSAGE = "간단 숫자 퍼즐";
     private static final String TURN = "Turn ";
-    public static void printStartMessage(){
+
+    public static void printStartMessage() {
         System.out.println(START_MESSAGE);
     }
-    public static void printTurn(Turn turn){
+
+    public static void printTurn(Turn turn) {
         System.out.println(TURN + turn.getTurn());
+    }
+
+    public static void printNumbers(Numbers numbers) {
+        System.out.println(numbers.getNumbers());
     }
 }
