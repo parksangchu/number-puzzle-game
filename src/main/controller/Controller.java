9543aa6d8f16b22e2adfebef1e6ee904545ae532
sangchu <didi1484@gmail.com>
2023-11-14 20:05:49 +0900

    feat: generateNumbers 메서드 생성
    시작값과 종료값을 입력받아 무작위 순서의 리스트 생성

--- src/main/controller/Controller.java
+++ src/main/controller/Controller.java
@@ -1,12 +1,15 @@
 package main.controller;
 
+import main.domain.Numbers;
+import main.domain.NumbersGenerator;
 import main.domain.Turn;
 import main.view.OutputView;
 
 public class Controller {
-    public void start(){
+    public void start() {
         OutputView.printStartMessage();
         Turn turn = new Turn();
         OutputView.printTurn(turn);
+        Numbers numbers = new Numbers(NumbersGenerator.generateNumbers(1, 8));
     }
 }
--- src/main/domain/NumbersGenerator.java
+++ src/main/domain/NumbersGenerator.java
@@ -7,9 +7,9 @@ import java.util.Random;
 public class NumbersGenerator {
     public static List<Integer> generateNumbers(int startNumber, int endNumber){
         Random random = new Random();
-        int randomNumber = random.nextInt(endNumber - startNumber + 1) + startNumber;
         List<Integer> Numbers = new ArrayList<>();
         while(isInvalidSize(Numbers,startNumber,endNumber)){
+            int randomNumber = random.nextInt(endNumber - startNumber + 1) + startNumber;
             if(!Numbers.contains(randomNumber)){
                 Numbers.add(randomNumber);
             }
