a77c4af51d65ad985bdbeb664a48c28cdf833797
sangchu <didi1484@gmail.com>
2023-11-14 20:34:07 +0900

    test: NumbersGenerator 추가

---
+++ src/test/domain/NumbersGeneratorTest.java
@@ -0,0 +1,20 @@
+package test.domain;
+
+import java.util.List;
+import main.domain.NumbersGenerator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NumbersGeneratorTest {
+
+    @ParameterizedTest
+    @DisplayName("1-8 수를 가진다.")
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8})
+    void generateNumbers(int input) {
+        List<Integer> numbers = NumbersGenerator.generateNumbers(1, 8);
+        Assertions.assertTrue(numbers.size() == 8);
+        Assertions.assertTrue(numbers.contains(input));
+    }
+}
