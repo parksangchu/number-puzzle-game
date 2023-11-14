5fe772ea01139bd48be9cc773c7487d59b81df62
sangchu <didi1484@gmail.com>
2023-11-14 17:46:38 +0900

    feat: increaseTurn 메서드 추가
    조건을 만족하면 턴을 증가시킴

--- src/main/domain/Turn.java
+++ src/main/domain/Turn.java
@@ -3,7 +3,11 @@ package main.domain;
 public class Turn {
     private int turn = 1;
 
+    public void increaseTurn(){
+        turn++;
+    }
     public int getTurn() {
         return turn;
     }
+
 }
