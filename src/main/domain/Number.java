a598f783973c9a6bdc33afc6a2f4751700735ee7
sangchu <didi1484@gmail.com>
2023-11-14 21:21:44 +0900

    feat: validateType 추가
    숫자 이외 입력시 예외 발생

--- src/main/domain/Number.java
+++ src/main/domain/Number.java
@@ -6,18 +6,31 @@ public class Number {
     public static final String INVALID_RANGE = "[ERROR] 1-8사이의 숫자만 입력 가능합니다.";
     private final int number;
 
-    public Number(int number) {
+    public Number(String number) {
+        validateType(number);
         validateRange(number);
-        this.number = number;
+        this.number = Integer.parseInt(number);
     }
 
-    private void validateRange(int number) {
-        if (!isValidRange(number)){
+    private void validateRange(String number) {
+        if (!isValidRange(number)) {
             throw new IllegalArgumentException(INVALID_RANGE);
         }
     }
-    private boolean isValidRange(int number) {
-        return number >= MIN_VALUE && number <= MAX_VALUE;
+
+    private boolean isValidRange(String number) {
+        return Integer.parseInt(number) >= MIN_VALUE && Integer.parseInt(number) <= MAX_VALUE;
+    }
+
+    private void validateType(String number) {
+        if (!isValidType(number)) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+    }
+
+    private boolean isValidType(String number) {
+        return number.chars()
+                .noneMatch(x -> !Character.isDigit(x));
     }
 
     public int getNumber() {
