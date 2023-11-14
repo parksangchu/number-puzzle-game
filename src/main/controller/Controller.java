0f2f1846f2c4525887a34f467d6b3863e91ce883
sangchu <didi1484@gmail.com>
2023-11-14 21:32:20 +0900

    feat: createExchangeNumber 추가

--- src/main/controller/Controller.java
+++ src/main/controller/Controller.java
@@ -1,8 +1,11 @@
 package main.controller;
 
+import java.util.List;
+import main.domain.Convertor;
 import main.domain.Numbers;
 import main.domain.NumbersGenerator;
 import main.domain.Turn;
+import main.view.InputView;
 import main.view.OutputView;
 
 public class Controller {
@@ -11,5 +14,18 @@ public class Controller {
         Turn turn = new Turn();
         OutputView.printTurn(turn);
         Numbers numbers = new Numbers(NumbersGenerator.generateNumbers(1, 8));
+        OutputView.printNumbers(numbers);
+        List<Integer> exchangeNumber = createExchangeNumber();
+    }
+
+    private List<Integer> createExchangeNumber() {
+        while (true) {
+            try {
+                String input = InputView.readExchangeNumber();
+                return Convertor.convertToExchangeNumbers(input);
+            } catch (IllegalArgumentException e) {
+                OutputView.printError(e);
+            }
+        }
     }
 }
