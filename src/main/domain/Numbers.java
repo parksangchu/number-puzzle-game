a506f4ecaf42148ee162b25f3e3c775d9d614c35
sangchu <didi1484@gmail.com>
2023-11-15 10:46:28 +0900

    feat: swapNumbers 메서드 추가
    교환할 번호를 받아 서로 스왑

--- src/main/domain/Numbers.java
+++ src/main/domain/Numbers.java
@@ -1,5 +1,6 @@
 package main.domain;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Numbers {
@@ -11,5 +12,15 @@ public class Numbers {
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    public void swapNumbers(List<Integer> exchangeNumbers) {
+        int indexZero = findIndex(exchangeNumbers, 0);
+        int indexOne = findIndex(exchangeNumbers, 1);
+        Collections.swap(numbers, indexZero, indexOne);
+    }
+
+    private int findIndex(List<Integer> exchangeNumbers, int index) {
+        return numbers.indexOf(exchangeNumbers.get(index));
     }
 }
