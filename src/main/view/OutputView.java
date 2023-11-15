877360771c71a48a3f43bcd700f9b6c9eb562bc7
sangchu <didi1484@gmail.com>
2023-11-15 14:18:31 +0900

    feat: printEnd 메서드 추가
    종료 메시지 출력

--- src/main/view/OutputView.java
+++ src/main/view/OutputView.java
@@ -6,6 +6,7 @@ import main.domain.Turn;
 public class OutputView {
     private static final String START_MESSAGE = "간단 숫자 퍼즐";
     private static final String TURN = "\nTurn ";
+    private static final String END = "%n%n축하합니다! %d턴만에 퍼즐을 완성하셨습니다!";
 
     public static void printError(Exception e) {
         System.out.println();
@@ -13,7 +14,7 @@ public class OutputView {
     }
 
     public static void printStartMessage() {
-        System.out.print(START_MESSAGE);
+        System.out.println(START_MESSAGE);
     }
 
     public static void printTurn(Turn turn) {
@@ -22,5 +23,9 @@ public class OutputView {
 
     public static void printNumbers(Numbers numbers) {
         System.out.println(numbers.getNumbers());
+    }
+
+    public static void printEnd(Turn turn) {
+        System.out.printf(END, turn.getTurn());
     }
 }
