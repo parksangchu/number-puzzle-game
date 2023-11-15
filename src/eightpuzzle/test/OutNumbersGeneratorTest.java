543769ea77cffb67940f5582c05a296ee9ac5581
sangchu <didi1484@gmail.com>
2023-11-15 18:18:45 +0900

    fix: 클래스명 수정

--- src/eightpuzzle/test/OutNumbersGeneratorTest.java
+++ src/eightpuzzle/test/OutNumbersGeneratorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class NumbersGeneratorTest {
+class OutNumbersGeneratorTest {
 
     @ParameterizedTest
     @DisplayName("1-8 수를 가진다.")
