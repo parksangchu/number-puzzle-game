ee0abb54c2308242b8b6420663ba53b8b8e75837
sangchu <didi1484@gmail.com>
2023-11-15 15:04:13 +0900

    feat: validateDuplicated 추가
    exchangeNumbers 중복 검증

--- src/main/domain/Convertor.java
+++ src/main/domain/Convertor.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 public class Convertor {
     private static final String DELIMITER = ",";
     private static final String INVALID_SIZE = "2개의 숫자를 입력해야 합니다.";
+    private static final String DUPLICATED = "중복된 숫자는 입력 불가능합니다.";
 
     public static List<Integer> convertToExchangeNumbers(String input) {
         List<Integer> exchangeNumbers = Arrays.stream(input.split(DELIMITER))
@@ -15,7 +16,21 @@ public class Convertor {
                 .map(Number::getNumber)
                 .collect(Collectors.toList());
         validateSize(exchangeNumbers);
+        validateDuplicated(exchangeNumbers);
         return exchangeNumbers;
+    }
+
+    private static void validateDuplicated(List<Integer> exchangeNumbers) {
+        if (isDuplicated(exchangeNumbers)) {
+            throw new IllegalArgumentException(DUPLICATED);
+        }
+    }
+
+    private static boolean isDuplicated(List<Integer> exchangeNumbers) {
+        int AfterCut = (int) exchangeNumbers.stream()
+                .distinct()
+                .count();
+        return exchangeNumbers.size() == AfterCut;
     }
 
     private static void validateSize(List<Integer> exchangeNumbers) {
