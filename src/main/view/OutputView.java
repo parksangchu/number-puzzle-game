33133615c0058c381fe5d629435257849a7fc692
sangchu <didi1484@gmail.com>
2023-11-15 14:30:19 +0900

    fix: 종료 메시지 띄어쓰기 버그 수정

--- src/main/view/OutputView.java
+++ src/main/view/OutputView.java
@@ -6,7 +6,7 @@ import main.domain.Turn;
 public class OutputView {
     private static final String START_MESSAGE = "간단 숫자 퍼즐";
     private static final String TURN = "\nTurn ";
-    private static final String END = "%n%n축하합니다! %d턴만에 퍼즐을 완성하셨습니다!";
+    private static final String END = "%n축하합니다! %d턴만에 퍼즐을 완성하셨습니다!";
 
     public static void printError(Exception e) {
         System.out.println();
