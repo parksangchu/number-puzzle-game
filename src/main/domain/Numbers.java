ec560edc667d725f85340605535182a4a36bf05f
sangchu <didi1484@gmail.com>
2023-11-15 14:16:26 +0900

    feat: isAnswer 메서드 추가
    정답 확인 기능

--- src/main/domain/Numbers.java
+++ src/main/domain/Numbers.java
@@ -2,6 +2,7 @@ package main.domain;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Numbers {
     private final List<Integer> numbers;
@@ -14,14 +15,20 @@ public class Numbers {
         return numbers;
     }
 
-    public void swapNumbers(List<Integer> exchangeNumbers, Turn turn) {
+    public void swapNumbers(List<Integer> exchangeNumbers) {
         int indexZero = findIndex(exchangeNumbers, 0);
         int indexOne = findIndex(exchangeNumbers, 1);
         Collections.swap(numbers, indexZero, indexOne);
-        turn.increaseTurn();
+    }
+
+    public boolean isAnswer() {
+        List<Integer> answer = List.of(1, 2, 3, 4, 5, 6, 7, 8);
+        return IntStream.range(0, numbers.size())
+                .allMatch(index -> numbers.get(index) == answer.get(index));
     }
 
     private int findIndex(List<Integer> exchangeNumbers, int index) {
         return numbers.indexOf(exchangeNumbers.get(index));
     }
+
 }
